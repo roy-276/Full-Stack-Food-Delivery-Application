e3733f92998e106f54f26282e4c15cb24372dec9
roy-276 <maingiroy95@gmail.com>
2022-09-15 09:14:44 +0300

    Completed

---
+++ Fudo/components/Services.jsx
@@ -0,0 +1,48 @@
+import css from '../styles/Services.module.css';
+import Image from 'next/image';
+import s1 from '../assets/s1.png';
+import s2 from '../assets/s2.png';
+import s3 from '../assets/s3.png';
+
+export default function Services() {
+    return(
+        <>
+        <div className={css.heading}>
+            <span>WHAT WE SERVE</span>
+            <span>Your Favourite Food</span>
+            <span>Delivery Partner</span>
+            </div>
+
+        {/* Features */}
+        <div className={css.services}>
+            <div className={css.feature}>
+
+                <div className={css.ImageWrapper}>
+                    <Image src={s1} alt="" objectFit='cover' layout='intrinsic'/>
+                    </div>
+
+                <span>Easy to Order</span>
+                <span>You only need a few steps in food ordering</span>
+            </div>
+
+            <div className={css.feature}>
+            <div className={css.ImageWrapper}>
+                    <Image src={s2} alt="" objectFit='cover' layout='intrinsic'/>
+                    </div>
+
+                <span>Easy to Order</span>
+                <span>Delivery that is Always on Time even Faster</span>
+            </div>
+
+            <div className={css.feature}>
+                <div className={css.ImageWrapper}>
+                    <Image src={s3} alt="" objectFit='cover' layout='intrinsic'/>
+                    </div>
+
+                <span>Easy to Order</span>
+                <span>Not only Fast for Us <br/> Quality is also NUmber One</span>
+            </div>
+        </div>
+        </>
+    )
+}
